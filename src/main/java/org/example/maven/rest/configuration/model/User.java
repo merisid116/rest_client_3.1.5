e065b6b5f8831ee444c16f852ac0f8aa108d4b64
Mari <merisid2399@mail.ru>
2022-05-27 22:23:32 +0300

    Initial commit

---
+++ src/main/java/org/example/maven/rest/configuration/model/User.java
@@ -0,0 +1,19 @@
+package org.example.maven.rest.configuration.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private Byte age;
+
+}
